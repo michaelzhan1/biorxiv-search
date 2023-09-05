487f1af9cd6a40eff1f03092bb94de495ec4bf4e
Michael Zhan <michaelzhan2001@gmail.com>
2023-09-05 09:31:40 -0400

    Style with tailwind

--- main/components/NewFilter.tsx
+++ main/components/NewFilter.tsx
@@ -81,15 +81,28 @@ export default function NewFilter () {
 
   return (
     <>
-      <form onSubmit={ handleSubmit }>
-        <label htmlFor="email">Email</label>
-        <input type="text" placeholder="Email" name="email" />
-        <label htmlFor="search">Search</label>
-        <input type="text" placeholder="Search" name="search" required/>
-        <label htmlFor="category">Categories</label>
-        <Select options={options} isMulti className='basic-multi-select' classNamePrefix='select' onChange={ handleSelectChange } />
-        <button type="submit">Set Filter</button>
-      </form>
+      <div className='flex flex-col h-screen justify-center items-center bg-gray-200'>
+        <div className="w-full max-w-sm">
+          <form onSubmit={ handleSubmit } className='bg-white rounded-md p-5 shadow-md'>
+            <h1 className='text-center text-xl font-bold text-gray-700 mb-4'>Set a bioRxiv search filter</h1>
+            <div className='mb-3'>
+              <label htmlFor="email" className='block font-bold text-sm text-gray-700 mb-2'>Email</label>
+              <input type="text" placeholder="Email" name="email"  className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+            </div>
+            <div className='mb-3'>
+              <label htmlFor="search" className='block font-bold text-sm text-gray-700 mb-2'>Search</label>
+              <input type="text" placeholder="Search" name="search" required className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+            </div>
+            <div className='mb-6'>
+              <label htmlFor="category" className='block font-bold text-sm text-gray-700 mb-2'>Categories</label>
+              <Select options={options} isMulti className='basic-multi-select' classNamePrefix='select' onChange={ handleSelectChange } />
+            </div>
+            <div className='flex justify-center'>
+              <button type="submit" className='bg-blue-500 hover:bg-blue-600 text-white rounded-md px-3 py-2 font-bold'>Set Filter</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
