84918589e781bd70dae1ad6c18f1980b3efaaa10
Michael Zhan <michaelzhan2001@gmail.com>
2025-01-22 20:27:51 -0500

    Refactor

---
+++ fix/backend/handlers/users.js
@@ -0,0 +1,70 @@
+import { createClient } from '../utils/db.js';
+
+export async function getUsers(req, res) {
+  const client = createClient();
+  await client.connect();
+  const result = await client.query('SELECT * FROM users');
+  await client.end();
+
+  res.json(result.rows);
+}
+
+export async function postUsers(req, res) {
+  const { email, search } = req.body;
+  const categories = req.body.categories.join(';');
+
+  if (!email) {
+    return res.status(400).json({ message: 'Missing email' });
+  }
+
+  let message;
+  let status = 200;
+
+  const client = createClient();
+  await client.connect();
+
+  try {
+    await client.query('BEGIN');
+    const existingUserRow = await client.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    );
+
+    if (existingUserRow.rowCount === 0) {
+      await client.query(
+        'INSERT INTO users (email, search, categories) VALUES ($1, $2, $3)',
+        [email, search, categories]
+      );
+      message = `User created with email: ${email}`;
+    } else {
+      await client.query(
+        'UPDATE users SET search = $1, categories = $2 WHERE email = $3',
+        [search, categories, email]
+      );
+      message = `User updated with email: ${email}`;
+    }
+    await client.query('COMMIT');
+  } catch (error) {
+    await client.query('ROLLBACK');
+    message = 'An error occurred';
+    status = 500;
+  } finally {
+    await client.end();
+  }
+
+  res.status(status).json({ message });
+}
+
+export async function deleteUsers(req, res) {
+  const id = parseInt(req.body.id);
+  if (!id) {
+    return res.status(400).json({ message: 'Invalid id' });
+  }
+
+  const client = createClient();
+  await client.connect();
+  await client.query('DELETE FROM users where id = $1', [id]);
+  await client.end();
+
+  res.json({ message: `User id ${id} deleted` });
+}
--- fix/backend/index.js
+++ fix/backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { createClient } from "./utils.js";
+import { getUsers, postUsers, deleteUsers } from "./handlers/users.js";
 
 dotenv.config();
 
@@ -11,79 +11,14 @@ app.use(express.json());
 
 // CORS setup
 const corsOptions = {
-  origin: 'http://localhost:8080'
+  origin: process.env.FRONTEND_URL,
 };
 app.use(cors(corsOptions));
 
 // Routes
-app.get('/users', async (req, res) => {
-  const client = createClient();
-  await client.connect();
-  const result = await client.query('SELECT * FROM users');
-  await client.end();
-
-  res.json(result.rows);
-})
-
-app.post('/users', async (req, res) => {
-  const { email, search } = req.body;
-  const categories = req.body.categories.join(';');
-
-  if (!email) {
-    return res.status(400).json({ message: 'Missing email' });
-  }
-
-  let message;
-  let status = 200;
-
-  const client = createClient();
-  await client.connect();
-
-  try {
-    await client.query('BEGIN');
-    const existingUserRow = await client.query(
-      'SELECT * FROM users WHERE email = $1',
-      [email]
-    );
-
-    if (existingUserRow.rowCount === 0) {
-      await client.query(
-        'INSERT INTO users (email, search, categories) VALUES ($1, $2, $3)',
-        [email, search, categories]
-      );
-      message = `User created with email: ${email}`;
-    } else {
-      await client.query(
-        'UPDATE users SET search = $1, categories = $2 WHERE email = $3',
-        [search, categories, email]
-      );
-      message = `User updated with email: ${email}`;
-    }
-    await client.query('COMMIT');
-  } catch (error) {
-    await client.query('ROLLBACK');
-    message = 'An error occurred';
-    status = 500;
-  } finally {
-    await client.end();
-  }
-
-  res.status(status).json({ message });
-});
-
-app.delete('/users', async (req, res) => {
-  const id = parseInt(req.body.id);
-  if (!id) {
-    return res.status(400).json({ message: 'Invalid id' });
-  }
-
-  const client = createClient();
-  await client.connect();
-  await client.query('DELETE FROM users where id = $1', [id]);
-  await client.end();
-
-  res.json({ message: `User id ${id} deleted` });
-})
+app.get('/users', getUsers);
+app.post('/users', postUsers);
+app.delete('/users', deleteUsers);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
