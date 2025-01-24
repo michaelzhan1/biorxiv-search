7140a45f540396d43095037e0b9790f7034f9dc5
Michael Zhan <michaelzhan2001@gmail.com>
2025-01-24 14:26:17 -0500

    Finish email

--- fix/backend/utils/email.js
+++ fix/backend/utils/email.js
@@ -19,30 +19,97 @@ const mailOptions = {
   to: '',
   subject: 'Weekly Biorxiv articles',
   html: '',
+  attachments: [
+    {
+      filename: 'biorxiv.png',
+      path: './assets/biorxiv.png',
+      cid: 'biorxiv'
+    }
+  ]
+}
+
+function makeEmailContent(user) {
+  const [startDate, endDate] = getDateRange();
+  const prettyStartDate = prettyFormat(startDate);
+  const prettyEndDate = prettyFormat(endDate);
+
+  return `
+    <html lang="en">
+
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>bioRxiv Email</title>
+    </head>
+
+    <body>
+      <table>
+        <tr>
+          <td>
+            <img src="cid:biorxiv" alt="bioRxiv logo" height="60px">
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p style="font-size: 16px;">
+              Click to view the latest bioRxiv articles from
+              <strong>${prettyStartDate}</strong>
+              to
+              <strong>${prettyEndDate}</strong>
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 10px;">
+            <a href="${process.env.FRONTEND_URL}/view?id=${user.id}"
+              style="background-color: #be2736;
+                     padding: 10px;
+                     margin-top: 10px;
+                     font-size: 16px;
+                     text-decoration: none;
+                     border-radius: 5px;
+                     color: white"
+            >
+              View Articles
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td style="padding-top: 15px;">
+            <a href="${process.env.FRONTEND_URL}/edit?id=${user.id}"
+              style="color: black;
+                     text-decoration: underline;
+                     font-size: 12px;"
+            >
+              Edit search preferences or unsubscribe
+            </a>
+          </td>
+        </tr>
+      </table>
+    </body>
+
+    </html>
+  `
 }
 
 // the emails don't need to include actual articles, just a link to the site
 async function sendEmails() {
   const allUsers = await getAllUserInfo();
-  const [startDate, endDate] = getDateRange();
-  const prettyStartDate = prettyFormat(startDate);
-  const prettyEndDate = prettyFormat(endDate);
 
   for (let user of allUsers) {
     mailOptions.to = user.email;
+    mailOptions.html = makeEmailContent(user);
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
   }
-
-
-  // dummy email for now
-  mailOptions.to = "sirmidgetmikl@gmail.com"
-  mailOptions.html = "new test email"
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent: " + info.response);
-    }
-  });
 }
+
+await sendEmails();
 
 export { sendEmails };
