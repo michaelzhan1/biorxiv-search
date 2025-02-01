6546a7f67a32ac1011f13319d602e07f4860935d
Michael Zhan <michaelzhan2001@gmail.com>
2025-01-31 23:31:54 -0500

    Update

--- fix/frontend/src/index.js
+++ fix/frontend/src/index.js
@@ -1,2 +1,15 @@
-const testDiv = document.getElementById(testDiv);
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const email = formData.get("email");
+  const keywords = formData.get("search");
+  const categories = formData.get("categories");
+  console.log(email, keywords, categories);
+}
+
+const form = document.getElementById("main-form");
+form.addEventListener("submit", handleSubmit);
+
+const testDiv = document.getElementById("testDiv");
 testDiv.innerHTML = process.env.TEST_MESSAGE;
