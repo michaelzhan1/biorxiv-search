04a34515f212650aef06d1cb5734112b83f8400b
Michael Zhan <michaelzhan2001@gmail.com>
2025-02-02 00:16:29 -0500

    Add API call for user

--- fix/frontend/src/index.js
+++ fix/frontend/src/index.js
@@ -3,13 +3,26 @@ function handleSubmit(e) {
 
   const formData = new FormData(e.target);
   const email = formData.get("email");
-  const keywords = formData.get("search");
-  const categories = formData.get("categories");
-  console.log(email, keywords, categories);
+  const search = formData.get("search");
+
+  fetch(process.env.API_URL + "/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      search: search,
+      categories: [],
+    }),
+  }).then((response) => {
+    if (response.ok) {
+      alert("User created/updated successfully");
+    } else {
+      alert("An error occurred");
+    }
+  });
 }
 
 const form = document.getElementById("main-form");
 form.addEventListener("submit", handleSubmit);
-
-const testDiv = document.getElementById("testDiv");
-testDiv.innerHTML = process.env.TEST_MESSAGE;
