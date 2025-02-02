dbbd6d9e3d2191f5837791f00849d24b01121d58
Michael Zhan <michaelzhan2001@gmail.com>
2025-02-02 10:04:26 -0500

    Easier data loading for dev

--- fix/backend/utils/biorxiv.js
+++ fix/backend/utils/biorxiv.js
@@ -1,5 +1,6 @@
 import { getAllUserInfo } from "./db.js";
 import { getDateRange } from "./date.js"
+import fs from "fs";
 
 // Object: key (user id) -> value (array of articles)
 let userArticles = {};
@@ -39,30 +40,41 @@ async function fetchAllArticles() {
 // Fetch all articles and filter them by user search terms and categories
 // for each user. Uses above function.
 async function fetchUserArticlesInPlace() {
-  const allArticles = await fetchAllArticles();
-  const allUsers = await getAllUserInfo();
+  if (process.env.NODE_ENV === 'development' && fs.existsSync('articles.json')) {
+    const articles = JSON.parse(fs.readFileSync('articles.json', 'utf8'));
+    userArticles = articles;
+    return
+  } else {
+    const allArticles = await fetchAllArticles();
+    const allUsers = await getAllUserInfo();
 
-  userArticles = {};
-  for (let user of allUsers) {
-    userArticles[user.id] = [];
-  }
-
-  for (let article of allArticles) {
+    userArticles = {};
     for (let user of allUsers) {
-      if (user.categories === null || user.categories.split(';').includes(article.category)) {
-        let wordCount = 0;
-        for (let word of user.search.split(';')) {
-          if (article.title.toLowerCase().includes(word.toLowerCase()) || article.abstract.toLowerCase().includes(word.toLowerCase())) {
-            wordCount++;
-          }
-        }
+      userArticles[user.id] = [];
+    }
 
-        if (wordCount >= user.search.split(';').length) {
-          userArticles[user.id].push(article);
+    for (let article of allArticles) {
+      for (let user of allUsers) {
+        if (user.categories === null || user.categories.split(';').includes(article.category)) {
+          let wordCount = 0;
+          for (let word of user.search.split(';')) {
+            if (article.title.toLowerCase().includes(word.toLowerCase()) || article.abstract.toLowerCase().includes(word.toLowerCase())) {
+              wordCount++;
+            }
+          }
+
+          if (wordCount >= user.search.split(';').length) {
+            userArticles[user.id].push(article);
+          }
         }
       }
     }
+
+    if (process.env.NODE_ENV === 'development') {
+      fs.writeFileSync('articles.json', JSON.stringify(userArticles));
+    }
   }
+
 }
 
 export { getUserArticles, fetchUserArticlesInPlace };
