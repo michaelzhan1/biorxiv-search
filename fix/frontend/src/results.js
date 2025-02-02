3054625c9038c0e50445db1667d277ce7524c6c4
Michael Zhan <michaelzhan2001@gmail.com>
2025-02-02 02:16:29 -0500

    Update results page

--- fix/frontend/src/results.js
+++ fix/frontend/src/results.js
@@ -1,6 +1,6 @@
 const numRegex = /^\d+$/;
 
-function onClick() {
+function getId() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
 
@@ -15,8 +15,45 @@ function onClick() {
   }
 
   const id = parseInt(urlParams.get("id"));
-  console.log("ID:", id);
+  return id;
 }
 
-const testButton = document.getElementById('test-button');
-testButton.addEventListener('click', onClick);
+const id = getId();
+
+// fetch user data
+fetch(`${process.env.API_URL}/users?id=${id}`)
+  .then((response) => response.json())
+  .then((userInfo) => {
+    if (userInfo.length === 0) {
+      alert("No user found");
+    }
+
+    const user = userInfo[0];
+    document.getElementById("email").innerHTML = user.email;
+    document.getElementById("search").innerHTML = user.search;
+    document.getElementById("categories").innerHTML = user.categories;
+  });
+
+
+// fetch articles
+fetch(`${process.env.API_URL}/data?id=${id}`)
+  .then((response) => response.json())
+  .then((articles) => {
+    if (articles.length === 0) {
+      alert("No articles found");
+    }
+
+    console.log(articles)
+
+    const articleContainer = document.getElementById("article-container");
+    articles.forEach((article) => {
+      const articleElement = document.createElement("div");
+      articleElement.innerHTML = `
+        <h2>${article.title}</h2>
+        <p>${article.authors}</p>
+        <a href="https://doi.org/${article.doi}">https://doi.org/${article.doi}</a>
+        <p>${article.abstract}</p>
+      `;
+      articleContainer.appendChild(articleElement);
+    });
+  });
