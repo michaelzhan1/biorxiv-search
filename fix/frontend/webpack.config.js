b5244604b2cf858b43af36c022d7dc3e3a20ad24
Michael Zhan <michaelzhan2001@gmail.com>
2025-02-02 21:24:42 -0500

    Restructure for webpack

--- fix/frontend/webpack.config.js
+++ fix/frontend/webpack.config.js
@@ -1,34 +1,36 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+// get javascript filenames and paths
+const jsFiles = fs.readdirSync(path.resolve(__dirname, 'src/js'));
+const entry = jsFiles.reduce((acc, filename) => {
+  const name = filename.split('.')[0];
+  acc[name] = path.resolve(__dirname, `src/js/${filename}`);
+  return acc;
+}, {});
+
+// get HTML filenames and paths
+const htmlFiles = fs.readdirSync(path.resolve(__dirname, 'src/html'));
+const htmlPlugins = htmlFiles.map((filename) => {
+  const name = filename.split('.')[0];
+  return new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, `src/html/${filename}`),
+    filename: filename,
+    chunks: [name],
+  });
+});
 
 module.exports = {
-  entry: {
-    main: ['./src/index.js'],
-    results: ['./src/results.js'],
-    preferences: ['./src/preferences.js'],
-  },
+  entry: entry,
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      chunks: ['main'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/results.html',
-      filename: 'results.html',
-      chunks: ['results'],
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/preferences.html',
-      filename: 'preferences.html',
-      chunks: ['preferences'],
-    }),
+    ...htmlPlugins,
   ],
   mode: 'development',
   devServer: {
@@ -37,6 +39,6 @@ module.exports = {
     },
     compress: true,
     port: 8080,
-    watchFiles: ['src/**/*.js'],
+    watchFiles: ['src/**/*.js', 'src/**/*.html'],
   }
 }
